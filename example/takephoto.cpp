74d9b727916c86abf75c7e0fd3406903cceb5eb5
Roman Senn <roman.l.senn@gmail.com>
2022-08-02 08:44:04 +0200

    ...

--- example/takephoto.cpp
+++ example/takephoto.cpp
@@ -7,9 +7,11 @@ main() {
   lccv::PiCamera cam;
   // cam.options->width=4056;
   // cam.options->height=3040;
-  cam.options->photo_width = 2028;
-  cam.options->photo_height = 1520;
-  cam.options->verbose = true;
+  //cam.options->photo_width = 2028;
+  //cam.options->photo_height = 1520;  
+  cam.options->photo_width = 2592;
+  cam.options->photo_height = 1944;
+   cam.options->verbose = true;
   cv::namedWindow("Image", cv::WINDOW_NORMAL);
   for(int i = 0; i < 100; i++) {
     std::cout << i << std::endl;
@@ -17,6 +19,7 @@ main() {
       std::cout << "Camera error" << std::endl;
     }
     cv::imshow("Image", image);
+    cv::imwrite("photo.png", image);
     cv::waitKey(30);
   }
   cv::waitKey();
